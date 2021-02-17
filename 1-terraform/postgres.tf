ba64886759461d21a0568b0ee4ab1344210ec71e
Mamta Jha <mamta.jha@techscalable.com>
2021-02-17 13:18:52 +0530

    c9

--- 1-terraform/postgres.tf
+++ 1-terraform/postgres.tf
@@ -1,10 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "postgresdb" {
   name     = var.resource_group
   location = var.azure_region
 }
 
 resource "azurerm_postgresql_server" "example" {
-  name                = var.psqlserver_name
+  server_name                = var.psqlserver_name
   location            = azurerm_resource_group.postgresdb.location
   resource_group_name = azurerm_resource_group.postgresdb.name
 
@@ -13,7 +17,7 @@ resource "azurerm_postgresql_server" "example" {
 
   sku_name   = "GP_Gen5_4"
   version    = "9.6"
-  storage_profile = "5120"
-  ssl_enforcement          = Enabled
+  storage_mb = "5120"
+  ssl_enforcement_enabled      = true
 }
 
