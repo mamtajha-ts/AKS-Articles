dfcdd735e64034b9ad4abb9fba4d470deb441d84
Mamta Jha <mamta.jha@techscalable.com>
2021-02-17 12:50:50 +0530

    c2

---
+++ 1-terraform/postgres.tf
@@ -0,0 +1,26 @@
+var.resource_groupresource "azurerm_resource_group" "postgresdb" {
+  name     = var.resource_group
+  location = var.azure_region
+}
+
+resource "azurerm_postgresql_server" "example" {
+  name                = var.psqlserver_name
+  location            = azurerm_resource_group.postgresdb.location
+  resource_group_name = azurerm_resource_group.postgresdb.name
+
+  administrator_login          = "psqladmin"
+  administrator_login_password = "Passw0rd@123j"
+
+  sku_name   = "GP_Gen5_4"
+  version    = "9.6"
+  storage_mb = 640000
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = true
+  auto_grow_enabled            = true
+
+  public_network_access_enabled    = false
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+
---
+++ 1-terraform/var-psql.tf
@@ -0,0 +1,21 @@
+variable "azure_region" {
+  type    = string
+  default = "eastus"
+}
+
+#  Resource Group Name
+variable "resource_group" {
+  type    = string
+  default = "AKSCluster-RG"
+}
+
+# Server name
+variable "psqlserver_name" {
+  type    = string
+  default = "AKSTerraformpsql"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "aksadmin"
+}
